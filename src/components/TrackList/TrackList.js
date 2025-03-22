af79656bdb8643252ec094f4f79bcc8753fa42e3
dylanbshell <dylan.shell@me.com>
2025-03-22 05:23:09 -0400

    Reset to blank template

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -5,21 +5,7 @@ import "./TrackList.css";
 import Track from "../Track/Track";
 
 const TrackList = (props) => {
-  return (
-    <div className="TrackList">
-      {props.tracks.map((track) => {
-        return (
-          <Track
-            track={track}
-            key={track.id}
-            onAdd={props.onAdd}
-            isRemoval={props.isRemoval}
-            onRemove={props.onRemove}
-          />
-        );
-      })}
-    </div>
-  );
+  
 };
 
 export default TrackList;
