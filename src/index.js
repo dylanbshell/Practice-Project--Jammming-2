679205ebbd7f27fb7fc64824ca13f56774466026
dylanbshell <dylan.shell@me.com>
2025-04-19 04:53:37 -0400

    Reset util and index

--- src/index.js
+++ src/index.js
@@ -6,9 +6,4 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = 
