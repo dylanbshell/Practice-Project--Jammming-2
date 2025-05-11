fdc4c1cd9e574f001348440bc11fef6a54c05c30
dylanbshell <dylan.shell@me.com>
2025-05-11 02:57:02 -0400

    index changes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App/App";
 import "./index.css";
 
-import ReactDOM from "react-dom/client";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
