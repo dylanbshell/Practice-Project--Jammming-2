f8520bf3d16767cd78426c91cbde629ebfa6a2d7
dylanbshell <dylan.shell@me.com>
2025-05-11 02:40:49 -0400

    search results component

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -6,7 +6,11 @@ import TrackList from "../TrackList/TrackList";
 
 const SearchResults = (props) => {
   return (
-
+    <div className="SearchResults">
+      <h2>Results</h2>
+      <TrackList tracks={props.searchResults} onAdd={props.onAdd} />
+    </div>
+  );
   )
 };
 
--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const root = 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
